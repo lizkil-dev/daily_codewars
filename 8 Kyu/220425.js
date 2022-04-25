983b3730878dc8960d7268eb9db2724ba3b39796
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-04-25 13:48:01 +0200

    Update 220425.js

--- 8 Kyu/220425.js
+++ 8 Kyu/220425.js
@@ -1,3 +1,18 @@
+/* Transportation on vacation
+   After a hard quarter in the office you decide to get some rest on a vacation.
+   So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+   You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+   Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.
+   Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+   
+   Write a code that gives out the total amount for different days(d).*/
+
+//My Solution//
+
+let rentalCarCost = d => d * 40 - ((d > 6) ? 50 : ((d > 2) ? 20 : 0));
+
+
+
 /* Difference of Volumes of Cuboids: 
    In this simple exercise, you will create a program that will take two lists of integers, a and b. 
    Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. 
