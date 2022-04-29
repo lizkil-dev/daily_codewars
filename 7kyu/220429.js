38848e31311a9aeb26839710727405f0cb045b5d
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-04-29 12:26:08 +0200

    Create 220429.js

---
+++ 7kyu/220429.js
@@ -0,0 +1,20 @@
+/* Sum of all the multiples of 3 or 5.
+  Your task is to write function findSum. Upto and including n, this function will return the sum of all multiples of 3 and 5.
+  For example:
+  findSum(5) should return 8 (3 + 5)
+  findSum(10) should return 33 (3 + 5 + 6 + 9 + 10) */
+
+// My Solution 
+
+
+const findSum = (n) => {
+  let multiples = [];
+  for(let i = 0; i <= n; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+      multiples.push(i); 
+    } 
+  }
+  multiples = multiples.reduce((a, b) => a+b, 0);
+  return multiples
+  
+}
