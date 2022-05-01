f3327d3663d61a6c1847a1d89e3cb48d04a9e694
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-01 14:36:21 +0200

    Create 220501.js

---
+++ 8 Kyu/220501.js
@@ -0,0 +1,8 @@
+/* Convert boolean values to strings 'Yes' or 'No'.
+   Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+
+//My Solution
+
+function boolToWord( bool ){
+  return bool ? 'Yes':'No';
+}
