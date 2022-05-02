baf33e19c9627b5611e09d941190cc27de5d4049
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-02 21:44:11 +0200

    Create 220502.js

---
+++ 8 Kyu/220502.js
@@ -0,0 +1,7 @@
+/* Convert a Number to a String!
+We need a function that can transform a number into a string.
+What ways of achieving this do you know? */
+
+// My Solution
+
+const numberToString = (num) => num.toString()
