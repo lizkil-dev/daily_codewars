567c9996d3468872e5bbaccf5f21a087e8e454bd
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-08 16:34:57 +0200

    Update 220508.js

--- 7kyu/220508.js
+++ 7kyu/220508.js
@@ -1,3 +1,23 @@
+/* Round up to the next multiple of 5
+   Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+   Examples:
+   input:    output:
+   0    ->   0
+   2    ->   5
+   3    ->   5
+   12   ->   15
+   21   ->   25
+   30   ->   30
+   -2   ->   0
+   -5   ->   -5
+  etc.  */
+
+//My Solution
+
+const roundToNext5 = (n) => Math.ceil(n/5) * 5
+
+
+
 /* Disemvowel Trolls
 
   Trolls are attacking your comment section!
