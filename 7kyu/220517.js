dadc652e930e2ae90cd55143aa48fb59ade24ba7
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-17 14:51:53 +0200

    Create 220517.js

---
+++ 7kyu/220517.js
@@ -0,0 +1,25 @@
+/* Create a function that takes an array of letters, and combines them into words in a sentence.
+  The array will be formatted as so:
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+
+The function should combine all the 0th indexed letters to create the word 'just',
+all the 1st indexed letters to create the word 'live', etc.
+Shorter words will have an empty string in the place once the word has already been mapped out (see the last element in the last element in the array). */
+
+
+// My Solution
+function arrAdder(arr) {
+ let result = "";
+
+ for(let i = 0; i < arr[0].length; i++){
+   for(let j = 0; j < arr.length; j++){
+   result = result + arr[j][i];
+   }
+   result+=" "
+ }
+ 
+ return result.trim();
+}
