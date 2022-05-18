a962c92714aad3deaeabc19fb7c6b876d1852049
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-18 12:44:21 +0200

    Update 220518.js

--- 7kyu/220518.js
+++ 7kyu/220518.js
@@ -10,3 +10,18 @@
 function filter_list(l) {
   return l.filter((e) => e = typeof = Number)  
 }
+
+
+/* Training on Mumbling
+This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"   */
+
+//My Solution
+function accum(s) {
+ return s.split('').map((e, i) => (e.toUpperCase() + e.toLowerCase().repeat(i))).join('-');
+}
+
