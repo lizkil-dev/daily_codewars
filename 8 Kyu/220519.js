e29cb8b40ab381d3d28eedb3c943cce45af486aa
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-19 08:33:56 +0200

    Update 220519.js

--- 8 Kyu/220519.js
+++ 8 Kyu/220519.js
@@ -7,3 +7,12 @@ const stringToNumber = function(str){
   
   return Number(str);
 }
+
+/* Return Negative
+  In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?*/
+
+//My Solution
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
