3ebbe29c545ab3201703fa77f7b5415e11429be2
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-22 00:18:53 +0200

    Create 220522.js

---
+++ 7kyu/220522.js
@@ -0,0 +1,17 @@
+/* Find the missing number
+
+The following was a question that I received during a technical interview for an entry level software developer position.
+I thought I'd post it here so that everyone could give it a go:
+You are given an unsorted array containing all the integers from 0 to 100 inclusively. 
+However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution? */
+
+//My Solution
+
+const missingNo = arr => {
+  arr.sort((a,b) => a-b);
+  for (let i = 0; i < arr.length+1; i++){
+    if(arr[i] !== i) {
+    return i;
+  }
+ }
+}
