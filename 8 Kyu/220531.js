706a1e8ee674aacf450c8c885f9699b2bbe61194
Lisa Kilbinger <102907651+lizkil-dev@users.noreply.github.com>
2022-05-31 20:00:20 +0200

    Update 220531.js

--- 8 Kyu/220531.js
+++ 8 Kyu/220531.js
@@ -1,3 +1,13 @@
+/* Calculate average
+Write a function which calculates the average of the numbers in a given list.
+Note: Empty arrays should return 0. */
+
+// My Solution
+function find_average(array) {
+ return array.length === 0 ? 0 : array.reduce((acc, c) => acc + c,0) / array.length
+  }
+
+
 /* Returning Strings
 Make a function that will return a greeting statement that uses an input;
 your program should return, "Hello, <name> how are you doing today?". */
