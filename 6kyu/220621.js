70852cae820497af258e06c581a107eb0d58c082
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-06-21 22:35:18 +0200

    add 7kyu to 220621.js

--- 6kyu/220621.js
+++ 6kyu/220621.js
@@ -11,3 +11,15 @@ function count (string) {
   return counts 
 }
 
+/*Shortest Word
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types. */
+
+function findShort(s){
+  let array = s.split(' ')
+  array.sort((a, b) => a.length - b.length)
+  return array[0].length
+}
+
+
