5e23f1a6b86b18806441fb94425cc51720c48195
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-06-27 14:21:18 +0200

    add 8kyu

--- 8 Kyu/220627.js
+++ 8 Kyu/220627.js
@@ -1,7 +1,15 @@
 /* Enumerable Magic #25 - Take the First N Elements
 Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.*/
 
-function take(arr, n) {
-  let newArr = arr.slice(0, n)
-  return newArr
+// function take(arr, n) {
+//   let newArr = arr.slice(0, n)
+//   return newArr
+// }
+
+/* String Templates - Bug Fixing #5
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!*/
+
+function buildString(...template){
+    return (`I like ${template.join(', ')}!`);
 }
+
