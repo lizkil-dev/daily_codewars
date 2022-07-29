f70ab46389557b5ce904450d3f25e85a917bec45
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-07-29 12:45:16 +0200

    changed solution

--- 5kyu/220719.js
+++ 5kyu/220719.js
@@ -6,31 +6,10 @@ pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 pigIt('Hello world !');     // elloHay orldway ! */
 
 function pigIt(str){
-
-  //divide str in multiple arrays, each array contains one word
-  let wordArray = str.split(' ')
-
-  let eachWord = []
-  wordArray.map((el) => eachWord.push(Array.from(el)))
-
-  // move first element of each word to last index
-  eachWord.map((el) => el.push(el.shift()))
+  let punctuation = ['.', ',', ':', '!', '?', ';']
+  let result = str.split(' ').map(word => word.slice(1) + word[0] + (punctuation.includes(word) ? '' : 'ay')).join(' ')
+  console.log(result);
   
-   // ad the letters ay at the end of the word
-   eachWord.forEach((el) => {if(el != '!' && el != '.' && el != ',' && el != '?' && el != ':'){
-    el.push('a', 'y')
-   }
-   })    
-
-  //bring words together again into one string with empty spaces between words
-  
-  let sentence = [...eachWord]
-
-  let string = sentence.join(' ')
-
-  let withoutCommas = string.replace(/,/g, '')
-  return withoutCommas
- 
 }
 
 pigIt('This is my string !')
