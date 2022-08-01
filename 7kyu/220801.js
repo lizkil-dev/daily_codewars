c6bb3f7960ba13fffe68eb92035ada3ee6d86d87
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-08-01 21:32:45 +0200

    add 7kyu

--- 7kyu/220801.js
+++ 7kyu/220801.js
@@ -19,3 +19,14 @@ function getDivisorsCnt(n){
     }
   return result
 }
+
+/* Number of decimal digit
+DESCRIPTION:
+Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid. */ 
+
+function digits(n) {
+  let result = n.toString().split('')
+  return result.length
+}
