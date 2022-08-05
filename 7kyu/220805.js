eb8954c80abdc6b583a20afa3d1708d32903794b
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-08-05 21:56:30 +0200

    updated 220805.js

--- 7kyu/220805.js
+++ 7kyu/220805.js
@@ -1,6 +1,5 @@
 /* Find the next perfect square!
 
-DESCRIPTION:
 You might know some pretty large perfect squares. But what about the NEXT one?
 
 Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
@@ -23,4 +22,3 @@ function findNextSquare(sq) {
   }  
 }
 
-findNextSquare(121) // 144
