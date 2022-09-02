5fc5bf0e672fc88aaaf8ab54a8ec511a4521ca20
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-09-02 12:00:00 +0200

    create 220901.js

--- 8 Kyu/220901.js
+++ 8 Kyu/220901.js
@@ -1,3 +1,9 @@
+/* Debug celsius converter
+Your friend is traveling abroad to the United States so he wrote a program to convert fahrenheit to celsius. Unfortunately his code has some bugs.
+
+Find the errors in the code to get the celsius converter working properly.*/
+
+
 function weatherInfo (temp) {
   var c = convertToCelsius(temp)
   if (c < 0)
