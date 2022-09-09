cfadbf9ad840cc14ebed940b2a3a108e9683dc9a
Lisa Kilbinger <lizkil.dev@gmail.com>
2022-09-09 12:00:00 +0200

    added description

--- 7kyu/220906.js
+++ 7kyu/220906.js
@@ -1,18 +1,27 @@
+/* DESCRIPTION:
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary) */
+
+
 function addBinary(a,b) {
   let quotient = a+b
   let binary = []
   
-
   while(quotient >= 1){
     let remainder = Math.floor(quotient) % 2
-    console.log(remainder);
     binary.unshift(remainder)
     quotient = Math.floor(quotient / 2)
   }
-  console.log(binary);
+  return binary
  }
 
 
- addBinary(5, 9) // 5 + 9 = 14 in decimal or 1110 in binary)
+ 
 
 
