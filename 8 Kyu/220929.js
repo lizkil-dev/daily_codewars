9b8708e9a30db05881f93d261d322e45f724de83
liz kil <lizkil.dev@gmail.com>
2022-09-30 12:10:44 +0200

    created 220929.js

--- 8 Kyu/220929.js
+++ 8 Kyu/220929.js
@@ -6,11 +6,7 @@ Copa del Rey
 Champions
 Complete the function to return his total number of goals in all three leagues.
 
-Note: the input will always be valid.
-
-For example:
-
-5, 10, 2  -->  17 */
+Note: the input will always be valid. */ 
 
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
