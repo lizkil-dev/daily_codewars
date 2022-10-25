c7d2f52abf65342026fea43ab033eb77964093bf
liz kil <lizkil.dev@gmail.com>
2022-10-25 14:18:16 +0200

    added 8kyu

--- 8 Kyu/221025.js
+++ 8 Kyu/221025.js
@@ -17,8 +17,15 @@ function fakeBin(x){
   });
 
   console.log(bin)
-  
-
 }
 
-fakeBin('45385593107843568') // '01011110001100111'
+/* You only need one 
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.*/
+  
+function check(a, x) {
+ return a.includes(x) ? true: false;
+}
