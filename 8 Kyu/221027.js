9b0a548291fe64f386d5e5f6a93758d7a213e48f
liz kil <lizkil.dev@gmail.com>
2022-10-27 17:34:06 +0200

    added 8kyu

--- 8 Kyu/221027.js
+++ 8 Kyu/221027.js
@@ -44,10 +44,14 @@ Return the average of the given array rounded down to its nearest integer.
 
 The array will never be empty.*/
 
-function getAverage(marks){
-  let sum = marks.reduce((a, b) => a+b)
-  let average = sum / marks.length
-  return Math.floor(average)
-}
+// function getAverage(marks){
+//   let sum = marks.reduce((a, b) => a+b)
+//   let average = sum / marks.length
+//   return Math.floor(average)
+// }
+
+or 
+
+const getAverage = marks => Math.floor((marks.reduce((a, b) => a+b)) / marks.length)
 
 getAverage([1,2,3,4,5,]) // 3
