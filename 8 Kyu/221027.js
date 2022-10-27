01b66714dd1fe9dc5b2b224de8e9409d8b1c3c78
liz kil <lizkil.dev@gmail.com>
2022-10-27 17:26:00 +0200

    added 8kyu

--- 8 Kyu/221027.js
+++ 8 Kyu/221027.js
@@ -19,3 +19,18 @@ const grow = x => console.log(x.reduce((a, b) => a*b))
 
 grow([2, 2, 2, 2, 2, 2]) // 64
 grow([1, 2, 3, 4])
+
+
+/* DESCRIPTION:
+Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great' */
+
+function smash(x){
+  console.log(x.join(' '))
+
+}
+
+smash(["this", "is", "a", "really", "long", "sentence"]) // "this is a really long sentence");
