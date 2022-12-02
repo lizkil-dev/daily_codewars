0ae93f157efefc4a79559d709853278ec5a4350b
liz kil <lizkil.dev@gmail.com>
2022-12-02 12:00:00 +0100

    created 221201.js

--- 8 Kyu/221201.js
+++ 8 Kyu/221201.js
@@ -1,6 +1,6 @@
 /* Sum Mixed Array
 Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
-Return your answer as a number. */
+Return your answer as a number */
 
 function sumMix(x){
   x.forEach (n => {n += n})
