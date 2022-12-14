ac123c7b01beaf8eacbafd34d912bc5f99d59d93
liz kil <lizkil.dev@gmail.com>
2022-12-14 06:52:17 -0800

    created 221129.js

--- 8 Kyu/221129.js
+++ 8 Kyu/221129.js
@@ -1,3 +1,6 @@
+/* Rock Paper Scissors */
+
+
 const rps = (p1, p2) => {
   if(p1 == "scissors" && p2 == "paper" || p1 == "paper" && p2 == "rock" || p1 == "rock" && p2 == "scissors"){
     return "Player 1 won!"
