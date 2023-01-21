da969e8e11eff3ab906a0938b2fc9f3883bc40e2
liz kil <lizkil.dev@gmail.com>
2023-01-20 22:33:11 -0800

    created 230120.js

--- 7kyu/230120.js
+++ 7kyu/230120.js
@@ -1,4 +1,12 @@
-/* Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2. */
+/* Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2. 
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"  */
 
 // function longest(s1, s2) {
 //   let s3 = new Set([...new Set(s1.split('')), ...new Set(s2.split(''))])
