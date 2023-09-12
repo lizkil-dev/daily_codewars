26db44c6c7fe5ddd63a4654baa9bb5b340797fb5
liz kil <lizkil.dev@gmail.com>
2023-09-12 12:21:50 +0300

    added 8kyu

---
+++ 8 Kyu/230912.js
@@ -0,0 +1,8 @@
+/* Enumerable Magic - Does My List Include This?
+
+DESCRIPTION:
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false. */ 
+
+function include(arr, item){
+  return arr.includes(item);
+}
